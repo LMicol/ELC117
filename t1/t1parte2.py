816ceabe8eab24ef550cf8b1ef5d97f251f29cd5
LMicol <lmpolicarpo@inf.ufsm.br>
2017-03-19 18:55:17 -0300

    Update t1parte2.py

--- t1/t1parte2.py
+++ t1/t1parte2.py
@@ -1,16 +1,16 @@
 # Parte 2 do trabalho de Paradigmas:
 
 # Q.1 : escreve sr. no inicio de cada string da lista
-list(map(lambda s: "Sr." + s, lista_de_nomes))
+def func1(lista_de_nomes):  return list(map(lambda s: "Sr." + s, lista_de_nomes))
 
 # Q.2 : faz a equação para cada valor da lista
-list(map((lambda n: ((3*n*2)+(2/n)+1)), lista_de_numeros))
+def func2(lista_de_numeros):  return list(map((lambda n: ((3*n*2)+(2/n)+1)), lista_de_numeros))
 
 # Q.3 : retorna uma lista que contem os nomes que terminam com "a"
-list(filter(lambda ult: ult[-1:] == "a", lista_de_nomes))
+def func3(lista_de_nomes):  return list(filter(lambda ult: ult[-1:] == "a", lista_de_nomes))
 
 # Q.4 : retorna uma lista das pessoas que nasceram depois de 1970
-list(filter(lambda num: num < 2017 - 1970, idades))
+def func4(idades):  return list(filter(lambda num: num < 2017 - 1970, idades))
 
 # Q.5 : o retorno da função será o new_numbers que é um vetor com os numeros de numbers * 2
-list(map(lambda num: num * 2, numbers)) 
+def func5(numbers):  return list(map(lambda num: num * 2, numbers)) 
